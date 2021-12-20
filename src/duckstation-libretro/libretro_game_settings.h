06c1d1d2aca77350ddc9d5de40e868a8c12ae756
Gregory Hainaut <gregory.hainaut@gmail.com>
2021-12-20 13:01:51 +0100

    options: add game code for games that support analog nicely

--- src/duckstation-libretro/libretro_game_settings.h
+++ src/duckstation-libretro/libretro_game_settings.h
@@ -2,3 +2,5 @@
 #include <memory>
 
 std::unique_ptr<GameSettings::Entry> GetSettingsForGame(const std::string& game_code);
+bool isAnalogOnlyGame(const std::string& game_code);
+bool isAnalogPreferedGame(const std::string& game_code);
